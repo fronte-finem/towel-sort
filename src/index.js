ce4ed6bf6c959b33370d3f38daa0629a8484b6ec
fronte-finem <fronte.finem+github@ya.ru>
2021-03-08 17:15:41 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    if (matrix == undefined || matrix.length == 0) return [];
+
+    return matrix
+        .map((row, i) => i % 2 ? row.reverse() : row)
+        .flat();
 }
